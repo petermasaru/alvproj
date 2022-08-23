7b7dd5a4da8120d50b704c1afaec11040d4c87a2
Peter Lee <peterjolee@gmail.com>
2022-08-23 12:25:15 -0700

    webapp update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import react, { useState } from 'react'
+import { sideLetterReps } from './CFCRep'
+import { LetterGenerator } from './letterGenerator'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ marginLeft: '2rem' }}>Tax Reps Generator v3</h1>
+      <p style={{ marginLeft: '2.5rem' }}><i>Please consult with the Tax Team before using this tool; it is still in beta.</i></p><br></br>
+      <div className='generator'>{LetterGenerator()}</div>
+    </div>
+  )
+}
+
+export default App;
