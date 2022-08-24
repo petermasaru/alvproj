a2103e26f5c96405c9b9d5524861d96be57dd441
Peter Lee <peterjolee@gmail.com>
2022-08-24 09:02:29 -0700

    changed page text (removed "text update" line).

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <h1 style={{ marginLeft: '2rem' }}>Tax Reps Generator v3</h1>
-      <p style={{ marginLeft: '2.5rem' }}><i>Please consult with the Tax Team before using this tool; it is still in beta. Test update here.</i></p><br></br>
+      <p style={{ marginLeft: '2.5rem' }}><i>Please consult with the Tax Team before using this tool; it is still in beta.</i></p><br></br>
       <div className='generator'>{LetterGenerator()}</div>
     </div>
   )
